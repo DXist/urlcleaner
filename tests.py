b213ce1561ed0738144965dfb2c3776a4830e4e1
Rinat Shigapov <rinatshigapov@gmail.com>
2015-06-21 23:01:01 +0300

    q based urlcleaner

--- tests.py
+++ tests.py
@@ -7,18 +7,39 @@ import logging
 import os
 import unittest
 
+import httpretty
 from urlcleaner import check_urls
 
 
-class TestCheckUrls(unittest.TestCase):
+
+class TestURLCleaner(unittest.TestCase):
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
+        self.addCleanup(self.loop.close)
+        self.urlcleaner = None
+
+    @httpretty.activate
     def test_ok(self):
         urls = [
             'https://twitter.com/anilkirbas',
             'https://www.twitter.com/rsk_living',
             'https://twitter.com/assaf'
         ]
+        for url in urls[:-1]:
+            httpretty.register_uri(httpretty.HEAD, url, status=200)
 
-        check_urls(urls)
+        httpretty.register_uri(urls[-1], url, status=404)
+
+        def urlreader():
+            yield from urls
+
+    def clean(self, **kwargs):
+        if self.urlcleaner:
+            self.urlcleaner.close()
+        self.urlcleaner = URLCleaner(urlreader=urlreader(), loop=self.loop)
+        self.addCleanup(urlcleaner.clean)
+        self.loop.run_until_complete(self.urlcleaner.clean)
 
 
 if __name__ == '__main__':
--- urlcleaner.py
+++ urlcleaner.py
@@ -1,30 +1,131 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.4
 # coding: utf-8
 
-"""Cleaner Twitter and Linkein urls"""
+"""Cleaner for Twitter and LinkedIn urls."""
+# requires python>=3.4.2
+# tested with aiohttp==0.16.5
 
 import asyncio
 import aiohttp
+import logging
+import time
+import urllib
+from collections import namedtuple
 
-MAX_CONNECTIONS = 30
+try:
+    # Python <3.4.4.
+    from asyncio import JoinableQueue as Queue
+except ImportError:
+    # Python >=3.4.4.
+    from asyncio import Queue
 
-@asyncio.coroutine
-def probe_url(session, url):
-    r = yield from session.head(url)
-    print(r.status, r.text(), url)
-    return (r.status, url)
+
+logger = logging.getLogger(__name__)
+
+
+URLStat = namedtuple('URLStat', ['url', 'next_url', 'status', 'exception'])
+
+
+def is_redirect(response):
+    return response.status in (300, 301, 302, 303, 307)
+
+
+class URLCleaner:
+    """Preprocess and clean Twitter and LinkedIn urls."""
+    def __init__(self, urls, max_connections=30, max_tasks=10,
+                 max_tries=4, qsize=100, *, loop=None):
+        self.loop = loop or asyncio.get_event_loop()
+        self.max_tasks = max_tasks
+        self.max_tries = max_tries
+        self.q = Queue(maxsize=qsize, loop=self.loop)
+        self.connector = aiohttp.TCPConnector(limit=max_connections, loop=loop)
+        self.urls = urls
+
+        self.t0 = time.time()
+        self.t1 = None
+
+    @asyncio.coroutine
+    def probe_url(self, url):
+        tries = 0
+        exception = None
+        while tries < self.max_tries:
+            try:
+                response = yield from aiohttp.request('head', url,
+                                                      allow_redirects=False,
+                                                      connector=self.connector,
+                                                      loop=self.loop)
+                if tries > 1:
+                    logger.info('Try %r for %r success', tries, url)
+                break
+            except aiohttp.ClientError as client_error:
+                logger.info('Try %r for %r raised %s', tries, url,
+                            client_error)
+                exception = client_error
+            tries += 1
+        else:
+            # all tries failed
+            logger.error('%r failed after %r', url, self.max_tries)
+            self.record_url_stat(URLStat(url=url, next_url=None, status=None,
+                                         exception=exception))
+            return
+
+        if is_redirect(response):
+            location = response.headers['location']
+            next_url = urllib.parse.urljoin(url, location)
+            self.record_url_stat(URLStat(url=url, next_url=next_url,
+                                         status=response.status,
+                                         exception=None))
+        else:
+            self.record_url_stat(URLStat(url=url, next_url=None,
+                                         status=response.status,
+                                         exception=None))
+            response.close()
+        return (response.status, url)
+
+    def close(self):
+        """Close resources."""
+        self.connector.close()
+
+    def record_url_stat(self, url_stat):
+        print(url_stat)
+
+    @asyncio.coroutine
+    def work(self):
+        """Process queue items forever."""
+        while True:
+            url = yield from self.q.get()
+            yield from self.probe_url(url)
+            self.q.task_done()
+
+
+    @asyncio.coroutine
+    def clean(self):
+        """Run the cleaner until all finished."""
+        workers = [asyncio.Task(self.work(), loop=self.loop)
+                   for _ in range(self.max_tasks)]
+        self.t0 = time.time()
+
+        for url in self.urls:
+            self.q.put(url)
+
+        yield from self.q.join()
+
+        self.t1 = time.time()
+        for w in workers:
+            w.cancel()
+
 
 
 if __name__ == '__main__':
-    conn = aiohttp.TCPConnector(limit=MAX_CONNECTIONS)
-    session = aiohttp.ClientSession(connector=conn)
 
-    with open('scoped_twitter_urls.txt') as f:
-        loop = asyncio.get_event_loop()
-        urls = [
-        'http://twitter.com/anilkirbas'
-        'http://www.twitter.com/rsk_living'
-        'http://twitter.com/assaf'
-        ]
-        f = asyncio.wait([probe_url(session, line.strip()) for line in urls])
-        loop.run_until_complete(f)
+    # with open('scoped_twitter_urls.txt') as f:
+    def filereader():
+        pass
+    loop = asyncio.get_event_loop()
+    urlcleaner = URLCleaner(urlreader=filereader(), loop=loop)
+
+    try:
+        loop.run_until_complete(urlcleaner)
+    finally:
+        urlcleaner.close()
+        loop.close()
