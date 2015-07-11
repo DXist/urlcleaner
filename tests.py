8a48c6e6e00dc67757ac4be26da84dce58a6c07e
Rinat Shigapov <rinatshigapov@gmail.com>
2015-07-11 22:28:34 +0300

    add twitter normalizer

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ import logging
 import os
 import unittest
 
-from urlcleaner import URLCleaner
+from urlcleaner import URLCleaner, URLStat, twitter_normalizer
 
 
 class TestURLCleaner(unittest.TestCase):
@@ -21,34 +21,92 @@ class TestURLCleaner(unittest.TestCase):
         self.urlcleaner = None
 
     def test_ok(self):
-        urls = [
-            'https://twitter.com/anilkirbas',
-            'https://www.twitter.com/rsk_living',
-            'https://twitter.com/assaf',
-        ]
-        self.clean(urls)
+        url_stat_apriori = {
+            '@anilkirbas': URLStat(
+                url='@anilkirbas',
+                local_clean_url='https://twitter.com/anilkirbas',
+                remote_clean_url='https://twitter.com/anilkirbas',
+                status='REMOTE_OK',
+                http_code=200,
+                exception=None
+            ),
+            'http://twitter.com/anilkirbas': URLStat(
+                url='http://twitter.com/anilkirbas',
+                local_clean_url='https://twitter.com/anilkirbas',
+                remote_clean_url='https://twitter.com/anilkirbas',
+                status='REMOTE_OK',
+                http_code=200,
+                exception=None
+            ),
+            'https://www.twitter.com/atweed': URLStat(
+                url='https://www.twitter.com/atweed',
+                local_clean_url='https://twitter.com/atweed',
+                remote_clean_url='https://twitter.com/atweed',
+                status='REMOTE_OK',
+                http_code=200,
+                exception=None
+            ),
+        }
+        self.clean(url_stat_apriori, normalizer=twitter_normalizer)
 
     def test_bad_data(self):
-        urls = [
-            '@anilkirbas',
-            'https://www.twitter.com/some_test_url',
-            'https://noname.noname',
-            'https://localhost:2',
-            None,
-        ]
-        self.clean(urls)
+        url_stat_apriori = {
+            'https://www.twitter.com/some_test_url': URLStat(
+                url='https://www.twitter.com/some_test_url',
+                local_clean_url='https://twitter.com/some_test_url',
+                remote_clean_url=None,
+                status='REMOTE_INVALID',
+                http_code=404,
+                exception=None
+            ),
+            'https://noname.noname': URLStat(
+                url='https://noname.noname',
+                local_clean_url=None,
+                remote_clean_url=None,
+                status='LOCAL_INVALID',
+                http_code=None,
+                exception=None
+            ),
+            'https://localhost:2': URLStat(
+                url='https://localhost:2',
+                local_clean_url=None,
+                remote_clean_url=None,
+                status='LOCAL_INVALID',
+                http_code=None,
+                exception=None
+            ),
+            None: URLStat(
+                url=None,
+                local_clean_url=None,
+                remote_clean_url=None,
+                status='LOCAL_INVALID',
+                http_code=None,
+                exception=None
+            ),
+        }
 
-    def clean(self, urls, **kwargs):
-        self.urlcleaner = URLCleaner(urls, loop=self.loop, **kwargs)
+        self.clean(url_stat_apriori, normalizer=twitter_normalizer,)
+
+    def clean(self, url_stat_apriori, normalizer, **kwargs):
+        num_of_successes = 0
+
+        def assert_result(urlstat):
+            nonlocal num_of_successes
+            apriori = url_stat_apriori[urlstat.url]
+            self.assertEqual(apriori, urlstat,
+                             '{} not equal to {}'.format(apriori, urlstat))
+            num_of_successes += 1 # noqa
+
+        self.urlcleaner = URLCleaner(url_stat_apriori, normalizer=normalizer,
+                                     loop=self.loop,
+                                     result_saver=assert_result, **kwargs)
         self.loop.run_until_complete(self.urlcleaner.clean())
+
+        self.assertEqual(len(url_stat_apriori), num_of_successes)
 
 
 if __name__ == '__main__':
     os.environ.setdefault('PYTHONASYNCIODEBUG', '1')
-    if os.environ.get('LOGLEVEL', 'DEBUG') == 'DEBUG':
-        loglevel = logging.DEBUG
-    else:
-        loglevel = logging.INFO
-
+    loglevel = os.environ.get('LOGLEVEL', 'DEBUG')
     logging.basicConfig(level=loglevel)
     unittest.main()
--- urlcleaner.py
+++ urlcleaner.py
@@ -7,8 +7,10 @@
 import asyncio
 import aiohttp
 import logging
+import re
 import time
-import urllib
+
+from urllib.parse import urlparse, urlunparse, urljoin
 
 try:
     # Python <3.4.4.
@@ -52,6 +54,15 @@ class URLStat:
                   self._allowed_attrs()]
         return 'URLStat({})'.format(', '.join(params))
 
+    def __eq__(self, other):
+        assert isinstance(other, URLStat)
+        for attr in self._allowed_attrs():
+            if getattr(self, attr) != getattr(other, attr):
+                logger.debug('%s attribute is different', attr)
+                return False
+
+        return True
+
 
 def is_redirect(response):
     return response.status in (300, 301, 302, 303, 307)
@@ -59,25 +70,32 @@ def is_redirect(response):
 
 class URLCleaner:
     """Preprocess and clean urls."""
-    def __init__(self, urls, max_connections=30, num_workers=1,
-                 max_tries=4, qsize=100, result_qsize=100, timeout=3, *,
-                 loop=None):
+    def __init__(self, urls, normalizer, result_saver=print,
+                 qsize=100, result_qsize=100, num_workers=1,
+                 max_tries=4, timeout=3, max_connections=30, *, loop=None):
+        self.urls = urls
+        self.normalizer = normalizer
+        self.result_saver = result_saver
+
         self.loop = loop or asyncio.get_event_loop()
-        self.num_workers = num_workers
-        self.max_tries = max_tries
         self.q = Queue(maxsize=qsize, loop=self.loop)
         self.result_q = Queue(maxsize=result_qsize, loop=self.loop)
+
+        self.num_workers = num_workers
+        self.max_tries = max_tries
         self.timeout = timeout
         self.connector = aiohttp.TCPConnector(limit=max_connections,
                                               loop=self.loop)
-        self.urls = urls
 
         self.t0 = time.time()
         self.t1 = None
 
     def local_clean(self, url):
-        local_clean_url = url
-        status = 'LOCAL_OK'
+        local_clean_url = self.normalizer(url)
+        if local_clean_url:
+            status = 'LOCAL_OK'
+        else:
+            status = 'LOCAL_INVALID'
         return URLStat(url=url, local_clean_url=local_clean_url,
                        remote_clean_url=None, status=status, http_code=None,
                        exception=None)
@@ -124,10 +142,11 @@ class URLCleaner:
 
         if is_redirect(response):
             location = response.headers['location']
-            remote_clean_url = urllib.parse.urljoin(url, location)
+            remote_clean_url = urljoin(url, location)
             urlstat.remote_clean_url = remote_clean_url
         elif response.status == 200:
             urlstat.status = 'REMOTE_OK'
+            urlstat.remote_clean_url = url
         else:
             urlstat.status = 'REMOTE_INVALID'
 
@@ -136,7 +155,7 @@ class URLCleaner:
     @asyncio.coroutine
     def process_url(self, url):
         urlstat = self.local_clean(url)
-        if urlstat.local_clean_url:
+        if urlstat.status == 'LOCAL_OK':
             urlstat = yield from self.remote_clean(urlstat)
         return urlstat
 
@@ -149,7 +168,11 @@ class URLCleaner:
         """Save cleaned URLStat."""
         while True:
             urlstat = yield from self.result_q.get()
-            print(urlstat)
+            try:
+                self.result_saver(urlstat)
+            except Exception as e:
+                logger.exception(e)
+
             self.result_q.task_done()
 
     @asyncio.coroutine
@@ -184,3 +207,55 @@ class URLCleaner:
             consumer.cancel()
         finally:
             self.close()
+
+
+def twitter_normalizer(url):
+    scheme, netloc, path, _, _, fragment = urlparse(url)
+    if scheme not in ('http', 'https', ''):
+        logger.debug('Invalid scheme %s, url %s', scheme, url)
+        return None
+
+    if netloc not in ('twitter.com', 'www.twitter.com', ''):
+        if netloc.startswith('@'):
+            # we have url like http://@nickname
+            nickname = netloc[1:]
+            return _twitter_url_from_nickname(nickname)
+        else:
+            logger.debug('Invalid netloc %s, url %s', netloc, url)
+            return None
+
+    nickname = _nickname_from_path(path)
+
+    if nickname:
+        # we have url like http://twitter.com/nickname
+        return _twitter_url_from_nickname(nickname)
+
+    if fragment:
+        # we have url like http://twitter.com/#!/nickname
+        if fragment.startswith('!/'):
+            nickname = fragment[2:]
+            return _twitter_url_from_nickname(nickname)
+
+    logger.debug('Invalid fragment %s, url %s', fragment, url)
+
+
+def _twitter_url_from_nickname(nickname):
+    if re.fullmatch(r'\w+', nickname, re.ASCII):
+        return urlunparse(
+            ('https', 'twitter.com', nickname, '', '', ''))
+    else:
+        logger.debug('Invalid nickname %s', nickname)
+        return None
+
+
+def _nickname_from_path(path):
+    if path.startswith('/'):
+        nickname = path[1:]
+    else:
+        nickname = path
+
+    if nickname.startswith('@'):
+        # we have url like http://twitter.com/@nickname
+        nickname = nickname[1:]
+
+    return nickname
